06d6b41897f5e32f8e8b6ac23a2ecf43005529d1
Mohamadreza-jabarzade <mr.jabarzade2005@gmail.com>
2026-06-02 15:57:13 +0330

    cros delete

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->prepend(\App\Http\Middleware\AuthenticateFromCookie::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
