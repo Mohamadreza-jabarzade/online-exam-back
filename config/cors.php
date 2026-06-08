e6b2e23358865b40ed97921386fc07549110bb7a
Mohamadreza-jabarzade <mr.jabarzade2005@gmail.com>
2026-06-08 19:36:06 +0330

    update cors policy

--- config/cors.php
+++ config/cors.php
@@ -2,18 +2,36 @@
 
 return [
 
-    'paths' => [
-        'api/*',
-        'sanctum/csrf-cookie',
-    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
+
 
     'allowed_origins' => [
         'http://localhost:3000',
         'http://localhost:5173',
         'http://5.61.31.89:3000',
         'http://5.61.31.89:9999',
+        'https://localhost:3000',
+        'https://localhost:5173',
+        'https://5.61.31.89:3000',
+        'https://5.61.31.89:9999',
+        'http://0.0.0.0:3000',
+        'http://0.0.0.0:5173',
+        'http://5.61.31.89',
     ],
 
     'allowed_origins_patterns' => [],
@@ -22,7 +40,7 @@ return [
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => true,
 
