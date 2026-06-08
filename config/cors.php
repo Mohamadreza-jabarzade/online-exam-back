f28ae3a051c40bb9ddea7cd646f6a971bb68fc05
Mohamadreza-jabarzade <mr.jabarzade2005@gmail.com>
2026-06-08 19:19:40 +0330

    update cors policy

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,19 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://5.61.31.89:3000',
+        'http://5.61.31.89:9999',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -16,6 +24,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
